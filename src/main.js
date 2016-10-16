21190e09400a54b866931ee6405e0373e0605a36
dllatas <danllatas@gmail.com>
2016-10-16 22:13:01 +0200

    First commit

---
+++ src/main.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// Use Bootstrap on the server
+import { Row, Col, Image, Jumbotron, FormControl, FormGroup, Button, Nav, NavItem } from 'react-bootstrap';
+import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
+
+class HeaderRow extends React.Component {
+    render() {
+        return (
+            <Row className="header-row">
+                <Col md={6} className="vertical-align header-row-img-left">
+                    <Image src="./bin/img/01.png" alt="" />
+                </Col>
+                <Col md={6} className="vertical-align text-right">
+                    <p className="header-row-text">SHELL PRIVATENERGIE</p>
+                    <Image src="./bin/img/02.png" alt="" />
+                </Col>
+            </Row>
+        )
+    }
+}
+
+class SearchRow extends React.Component {
+    render() {
+        return (
+            <Row className="search-row">
+                <Col md={12}>
+                    <p className="text-center search-row-p">Berechnen Sie Ihren individuellen Tarif:</p>
+                </Col>
+                <Col md={4} className="search-row-input vertical-align">
+                    <FormControl type="text" placeholder="Ihre Postleitzahl" />
+                </Col>
+                <Col md={4} className="search-row-select vertical-align">
+                    <FormGroup controlId="formControlsSelect">
+                        <FormControl componentClass="select" placeholder="select">
+                            <option value="select">Ihr Jahresverbrauch</option>
+                        </FormControl>
+                    </FormGroup>
+                </Col>
+                <Col md={4} className="search-row-col-button vertical-align">
+                    <Button className="search-row-button">Jetzt Tarif berechnen</Button>
+                </Col>
+            </Row>
+        )
+    }
+}
+
+class DisclaimerRow extends React.Component {
+    render() {
+        return (
+            <Row className="disclaimer-row">
+                <Col md={12}>
+                    <p className="text-center disclaimer-row-text">Shell PrivatEnergie (Strom-und Gasprodukte fur Privathaushalte) ist ein Angebot der First Utility GmbH. First Utility GmbH nutzt Shell Marken unter Lizenzvereinbarung.</p>
+                </Col>
+            </Row>
+        )
+    }
+}
+
+class FooterRow extends React.Component {
+    render() {
+        return (
+            <Row className="footer-row">
+                <Col md={12}>
+                    <Nav bsStyle="pills">
+                        <NavItem className="nav-item-footer">Uber uns</NavItem>
+                        <NavItem className="nav-item-footer">Kontakt</NavItem>
+                        <NavItem className="nav-item-footer">Energiemix</NavItem>
+                        <NavItem className="nav-item-footer">AGB</NavItem>
+                        <NavItem className="nav-item-footer">Datenschutz</NavItem>
+                        <NavItem className="nav-item-footer">Impressum</NavItem>
+                    </Nav>
+                </Col>
+            </Row>
+        )
+    }
+}
+
+class Container extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <HeaderRow />
+                <Jumbotron />
+                <SearchRow />
+                <DisclaimerRow />
+                <FooterRow />
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<Container />, document.getElementsByTagName("BODY")[0]);
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+         path: './bin',
+         filename: 'main.bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader"
+            },
+            {
+                test: /\.png$/,
+                loader: "url-loader?limit=100000"
+            },
+            {
+                test: /\.jpg$/,
+                loader: "file-loader"
+            },
+            {
+                test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=application/font-woff'
+            },
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=application/octet-stream'
+            },
+            {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'file'
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=image/svg+xml'
+            }
+        ]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false,
+            },
+            output: {
+                comments: false,
+            },
+        }),
+    ]
+ };
